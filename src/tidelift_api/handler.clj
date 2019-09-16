f9e725ab0eb15912be036eb8f52d886a0117cb50
trinityXmontoya <trinityXmontoya@gmail.com>
2019-09-16 16:47:49 -0600

    first pass, refactor

--- src/tidelift_api/handler.clj
+++ src/tidelift_api/handler.clj
@@ -7,13 +7,18 @@
             [semantic-csv.core :as sc]
             [clj-http.client :as http-client]))
 
-(s/defschema Pizza
-  {:name s/Str
-   (s/optional-key :description) s/Str
-   :size (s/enum :L :M :S)
-   :origin {:country (s/enum :FI :PO)
-            :city s/Str}})
+; ------------
+; SWAGGER DEFINITION
+; ------------
+(def swagger-definition
+  {:swagger
+   {:ui "/"
+    :spec "/swagger.json"
+    :data {:info {:title "Tidelift API"}}}})
 
+; ------------
+; SCHEMAS
+; ------------
 (s/defschema PackageVulnerability
   {:id s/Str
    :description s/Str
@@ -30,118 +35,133 @@
    :latest s/Str
    :releases [s/Str]})
 
-(defn find-package-vulnerabilities
-  [package_name version]
+; ------------
+; HELPER FNS
+; ------------
+(defn filter-csv
+  "reads the CSV at the given path into a lazy sequence of rows mapped to the given headers.
+   then, filters that sequence by applying the given filter_fn"
+  [path headers filter_fn]
   (filter
-    (fn [vulnerability]
-      (and (= (vulnerability :package_name) package_name)
-           (= (vulnerability :package_version) version)))
-    (with-open [in-file (io/reader "/Users/maybetrinity/Desktop/moi/jobs/tidelift-api/src/tidelift_api/database/vulnerabilities.csv")]
+    #(filter_fn %)
+    (with-open [in-file (io/reader (io/resource path))]
       (->>
         (csv/parse-csv in-file)
-        (sc/mappify {:header [:id :package_name :package_version :description :created]})
+        (sc/mappify {:header headers})
         doall))))
 
-(defn find-package-license
+; ------------
+; VULNERABILITIES
+; ------------
+(def vulnerabilities_csv_path "resources/database/vulnerabilities.csv")
+(def vulnerabilities_csv_headers [:id :package_name :package_version :description :created])
+
+(defn vulnerability-matches-package-name-and-version?
+  [vulnerability package_name version]
+  (and (= (get vulnerability :package_name) package_name)
+       (= (get vulnerability :package_version) version)))
+
+(defn find-package-vulnerabilities
+  [package_name version]
+  (filter-csv
+    vulnerabilities_csv_path
+    vulnerabilities_csv_headers
+    (fn [vulnerability] (vulnerability-matches-package-name-and-version? vulnerability package_name version))))
+
+; ------------
+; LICENSES
+; ------------
+(def licenses_csv_path "resources/database/licenses.csv")
+(def licenses_csv_headers [:package_name :license])
+
+(defn license-info-matches-package-name?
+  [license_info package_name]
+  (= (get license_info :package_name) package_name))
+
+(defn find-package-license-info
   [package_name]
   (first
-  (filter
-    (fn [license_info] (= (license_info :package_name) package_name))
-    (with-open [in-file (io/reader "/Users/maybetrinity/Desktop/moi/jobs/tidelift-api/src/tidelift_api/database/licenses.csv")]
-      (->>
-        (csv/parse-csv in-file)
-        (sc/mappify {:header [:package_name :license]})
-        doall)))))
+    (filter-csv
+      licenses_csv_path
+      licenses_csv_headers
+      (fn [license-info] (license-info-matches-package-name? license-info package_name)))))
+
+; ------------
+; NPM REGISTRY SEARCH
+; ------------
+(def npm_registry_url "https://registry.npmjs.org/")
+(def npm_abbreviated_json_header "application/vnd.npm.install-v1+json")
+(def npm_registry_headers {:headers {:accept npm_abbreviated_json_header}
+                           :throw-entire-message? true
+                           :as :json})
+
+(defn get_npm_release_info
+  [package_name]
+  (let [request_url (str npm_registry_url package_name)]
+    (try
+      (let [response (http-client/get request_url npm_registry_headers)]
+        (response :body))
+      (catch Exception e
+        (str "oh no" e)
+        nil))))
+
+(defn format-npm-release-info
+  [package_name npm_release_info]
+  {:name package_name
+   :latest (get-in npm_release_info [:dist-tags :latest])
+   :releases  (map #((second %) :version) (get npm_release_info :versions))})
+
+; ------------
+; PACKAGE HEALTH
+; ------------
+(defn format-license-info
+  [license-info]
+  (get license-info :license))
+
+(defn format-vulnerabilities
+  [vulnerabilities]
+  (map #(select-keys % [:id :description  :created]) vulnerabilities))
 
 (defn build-package-health
   [package_name version]
-  (if-let [license (get (find-package-license package_name) :license)]
-    (if-let [vulnerabilities (find-package-vulnerabilities package_name version)]
+  (if-let [formatted-license (format-license-info (find-package-license-info package_name))]
+    (if-let [formatted-vulnerabilities (format-vulnerabilities (find-package-vulnerabilities package_name version))]
       {:name package_name
        :version version
-       :license license
-       :vulnerabilities (map #(select-keys % [:id :description :created]) vulnerabilities)})))
+       :license formatted-license
+       :vulnerabilities formatted-vulnerabilities})))
 
+; ------------
+; RELEASES
+; ------------
 (defn build-package-releases
   [package_name]
-  (let [request_url (str "https://registry.npmjs.org/" package_name)]
+  (if-let [npm_release_info (get_npm_release_info package_name)]
+    (format-npm-release-info package_name npm_release_info)))
 
-    (try
-      ;application/vnd.npm.install-v1+json
-      (let [response (http-client/get request_url {:throw-entire-message? true
-                                                   :accept :json
-                                                   :as :json})]
-
-        {:name package_name
-         :latest (get-in response [:body :dist-tags :latest])
-         :releases  (map #((second %) :version) (get-in response [:body :versions]))})
-
-      (catch Exception e
-        (str "oh no" e)
-        nil))
-
-
-
-    )
-
-
-
-
-  )
-
+; ------------
+; MAIN
+; ------------
 (def app
   (api
-    {:swagger
-     {:ui "/"
-      :spec "/swagger.json"
-      :data {:info {:title "Tidelift API"}
-             :tags [{:name "api", :description "some apis"}]}}}
-
+    swagger-definition
     (context "/" []
-      :tags [""]
 
-      (GET "/package/health/:package_name/:version" []
-        :return PackageHealth
-        :path-params [package_name :- s/Str, version :- s/Str]
-        :summary "a json dictionary for that package containing information about the license and security vulnerabilities in that version of that package"
-        (if-let [package_health (build-package-health package_name version)]
-          (ok package_health)
-          (not-found (str "There were no vulnerabilities found for package_name " package_name " version " version))))
+      (context "/package" []
 
-      (GET "/package/releases/:package_name" []
-        :return PackageReleases
-        :path-params [package_name :- s/Str]
-        :summary "a list of the versions available from npm (from the versions in the response from npmjs) and latest should be the most recently published version based on looking at the values of the time dictionary in the response from npmjs"
-        (if-let [package_releases (build-package-releases package_name)]
-          (ok package_releases)
-          (not-found (str "There were no releases found for package_name " package_name))))
+        (GET "/health/:package_name/:version" []
+          :return PackageHealth
+          :path-params [package_name :- s/Str
+                        version :- s/Str]
+          :summary "a json dictionary for that package containing information about the license and security vulnerabilities in that version of that package"
+          (if-let [package_health (build-package-health package_name version)]
+            (ok package_health)
+            (not-found (str "There were no vulnerabilities found for package_name " package_name " version " version))))
 
-
-      )
-
-    ))
-
-
-;(def app
-;  (api
-;    {:swagger
-;     {:ui "/"
-;      :spec "/swagger.json"
-;      :data {:info {:title "Tidelift-api"
-;                    :description "Compojure Api example"}
-;             :tags [{:name "api", :description "some apis"}]}}}
-;
-;    (context "/api" []
-;      :tags ["api"]
-;
-;      (GET "/plus" []
-;        :return {:result Long}
-;        :query-params [x :- Long, y :- Long]
-;        :summary "adds two numbers together"
-;        (ok {:result (+ x y)}))
-;
-;      (POST "/echo" []
-;        :return Pizza
-;        :body [pizza Pizza]
-;        :summary "echoes a Pizza"
-;        (ok pizza)))))
+        (GET "/releases/:package_name" []
+          :return PackageReleases
+          :path-params [package_name :- s/Str]
+          :summary "a list of the versions available from npm (from the versions in the response from npmjs) and latest should be the most recently published version based on looking at the values of the time dictionary in the response from npmjs"
+          (if-let [package_releases (build-package-releases package_name)]
+            (ok package_releases)
+            (not-found (str "There were no releases found for package_name " package_name))))))))
